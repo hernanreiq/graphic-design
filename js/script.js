ae19476e3be0a63bdfec081f02f9009b7d506d37
Reiq.Laptop <reiqvideos@gmail.com>
2021-06-12 02:10:45 -0400

    Proyecto Golden Lottery agregado, pulido el diseño y presentacion de las tarjetas

--- js/script.js
+++ js/script.js
@@ -4,29 +4,45 @@ var ventana_emergente_texto = document.getElementById('VentanaEmergenteTexto');
 
 var proyectos = [{
   nombre: 'Ed Stylish',
-  tipo_proyecto: 'Tienda virtual de ropa pre-amada y nueva',
-  cantidad_fotos: 6
+  tipo_proyecto: 'Tienda virtual de ropa pre-amada y nueva.',
+  cantidad_fotos: 6,
+  formato: 'jpg'
+},{
+  nombre: 'Golden Lottery',
+  tipo_proyecto: 'Composición web de una lotería virtual.',
+  cantidad_fotos: 1,
+  formato: 'jpg'
 },{
   nombre: 'El Piloto Tour',
-  tipo_proyecto: 'Grupo de turismo en República Dominicana',
-  cantidad_fotos: 5
+  tipo_proyecto: 'Grupo de turismo en República Dominicana.',
+  cantidad_fotos: 5,
+  formato: 'jpg'
 }];
 
 function creadorDeTarjetas(){
   var cantidad_proyectos = proyectos.length;
   for(var i = 0; i < cantidad_proyectos; i++){
+    if(proyectos[i].cantidad_fotos > 1){
+      var textoBoton = '<b>Ver diseños</b>';
+    } else {
+      var textoBoton = '<b>Ver diseño</b>';
+    }
     contenedor_tarjetas.innerHTML += `
-    <div class="elemento card">
-        <img src="img/proyectos/${proyectos[i].nombre}/0.jpg" alt="${proyectos[i].nombre}">
-        <div class="card-body">
-            <h5 class="card-title">${proyectos[i].nombre}</h5>
-            <p class="card-text">${proyectos[i].tipo_proyecto}</p>
-            <button onclick="verImagenes(${i})" type="button" class="btn btn-primary" data-toggle="modal" data-target="#VentanaEmergente">Ver diseños</button>
-        </div>    
-    </div>
+      <div class="elemento card">
+          <div class="img-card">
+            <img src="img/proyectos/${proyectos[i].nombre}/0.${proyectos[i].formato}" class="card-img-top" alt="${proyectos[i].nombre}">
+          </div>
+          <div class="card-body">
+              <h5 class="card-title">${proyectos[i].nombre}</h5>
+              <p class="card-text">${proyectos[i].tipo_proyecto}</p>
+              <button onclick="verImagenes(${i})" type="button" class="btn btn-color" data-toggle="modal" data-target="#VentanaEmergente">${textoBoton} <span class="badge badge-primary">${proyectos[i].cantidad_fotos}</span></button>
+          </div>    
+      </div>
     `;
   }
+  console.log('Tarjetas creadas!');
 }
+
 creadorDeTarjetas();
 
 function verImagenes(numero_proyecto){
@@ -40,7 +56,7 @@ function verImagenes(numero_proyecto){
     }
     fotos_carrusel.innerHTML += `
     <div class="carousel-item ${primeraFoto}">
-      <img src="img/proyectos/${proyectos[numero_proyecto].nombre}/${i}.jpg" class="d-block fotos-modal" alt="${proyectos[numero_proyecto].nombre}">
+      <img src="img/proyectos/${proyectos[numero_proyecto].nombre}/${i}.${proyectos[numero_proyecto].formato}" class="d-block fotos-modal" alt="${proyectos[numero_proyecto].nombre}">
     </div>
     `;
   }
@@ -51,4 +67,4 @@ var elem = document.querySelector('#contenedor-tarjetas');
 var msnry = new Masonry( elem, {
   itemSelector: '.elemento'
 });
-var msnry = new Masonry( '#contenedor-tarjetas', {});
+console.log('Efecto Masonry activado!');
